e0fb46848d380e39342523360cfa655c4256b980
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-09-04 13:05:11 +0100

    - removed m_ from struct members

--- EllieSpeed.Interfaces/GPBikes.cs
+++ EllieSpeed.Interfaces/GPBikes.cs
@@ -16,169 +16,169 @@ namespace EllieSpeed.Interfaces
     public struct SPluginsBikeEvent_t
     {
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szRiderName;
+      public string RiderName;
 
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szBikeID;
+      public string BikeID;
 
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szBikeName;
+      public string BikeName;
 
-      public int m_iNumberOfGears;
+      public int NumberOfGears;
 
-      public float m_fMaxRPM;
+      public float MaxRPM;
 
-      public float m_fLimiter;
+      public float Limiter;
 
-      public float m_fShiftRPM;
+      public float ShiftRPM;
 
       /*  degrees Celsius  */
-      public float m_fEngineOptTemperature;
+      public float EngineOptTemperature;
 
       /*  degrees Celsius. Lower and upper limits  */
       [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 2, ArraySubType = UnmanagedType.R4)]
-      public float[] m_afEngineTemperatureAlarm;
+      public float[] EngineTemperatureAlarm;
 
       /*  liters  */
-      public float m_fMaxFuel;
+      public float MaxFuel;
 
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szCategory;
+      public string Category;
 
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szTrackID;
+      public string TrackID;
 
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szTrackName;
+      public string TrackName;
 
       /*  centerline length. meters  */
-      public float m_fTrackLength;
+      public float TrackLength;
     }
 
     [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct SPluginsBikeSession_t
     {
       /*  0 = testing; 1 = practice; 2 = qualify; 3 = warmup; 4 = race  */
-      public int m_iSession;
+      public int Session;
 
       /*  0 = sunny; 1 = cloudy; 2 = rainy  */
-      public int m_iConditions;
+      public int Conditions;
 
       /*  degrees Celsius  */
-      public float m_fAirTemperature;
+      public float AirTemperature;
 
       /*  degrees Celsius  */
-      public float m_fTrackTemperature;
+      public float TrackTemperature;
 
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szSetupFileName;
+      public string SetupFileName;
     }
 
     [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct SPluginsBikeData_t
     {
       /*  engine rpm  */
-      public float m_fRPM;
+      public float RPM;
 
       /*  degrees Celsius  */
-      public float m_fEngineTemperature;
+      public float EngineTemperature;
 
       /*  degrees Celsius  */
-      public float m_fWaterTemperature;
+      public float WaterTemperature;
 
       /*  0 = Neutral  */
-      public int m_iGear;
+      public int Gear;
 
      /*  liters  */
-      public float m_fFuel;
+      public float Fuel;
 
       /*  meters/second  */
-      public float m_fSpeedometer;
+      public float Speedometer;
 
       /*  world position of a reference point attached to chassis ( not CG )  */
-      public float m_fPosX;
-      public float m_fPosY;
-      public float m_fPosZ;
+      public float PosX;
+      public float PosY;
+      public float PosZ;
 
       /*  velocity of CG  in world coordinates. meters/second  */
-      public float m_fVelocityX;
-      public float m_fVelocityY;
-      public float m_fVelocityZ;
+      public float VelocityX;
+      public float VelocityY;
+      public float VelocityZ;
 
       /*  acceleration of CG  local to chassis rotation expressed in G ( 9.81 m/s2 ) and averaged over the latest 10ms  */
-      public float m_fAccelerationX;
-      public float m_fAccelerationY;
-      public float m_fAccelerationZ;
+      public float AccelerationX;
+      public float AccelerationY;
+      public float AccelerationZ;
 
       /*  rotation  matrix of the chassis.  It incorporates lean and wheeling  */
       [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 9, ArraySubType = UnmanagedType.R4)]
-      public float[] m_aafRot;
+      public float[] Rot;
 
       /*  degrees,  -180  to  180  */
-      public float m_fYaw;
-      public float m_fPitch;
-      public float m_fRoll;
+      public float Yaw;
+      public float Pitch;
+      public float Roll;
 
       /*  degress / second  */
-      public float m_fYawVelocity;
-      public float m_fPitchVelocity;
-      public float m_fRollVelocity;
+      public float YawVelocity;
+      public float PitchVelocity;
+      public float RollVelocity;
 
       /*  normalized suspensions length.  0 = front; 1 = rear  */
       [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 2, ArraySubType = UnmanagedType.R4)]
-      public float[] m_afSuspNormLength;
+      public float[] SuspNormLength;
 
       /*  1 = rider is detached from bike  */
-      public int m_iCrashed;
+      public int Crashed;
 
       /*  degrees.  Negative = right  */
-      public float m_fSteer;
+      public float Steer;
 
       /*  0  to  1  */
-      public float m_fThrottle;
+      public float Throttle;
 
       /*  0  to  1  */
-      public float m_fFrontBrake;
+      public float FrontBrake;
 
       /*  0  to  1  */
-      public float m_fRearBrake;
+      public float RearBrake;
 
       /*  0  to  1.  0 =  Fully engaged  */
-      public float m_fClutch;
+      public float Clutch;
 
       /*  meters/second.  0 = front; 1  = rear  */
       [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 2, ArraySubType = UnmanagedType.R4)]
-      public float[] m_afWheelSpeed;
+      public float[] WheelSpeed;
 
       /*  1 = pit limiter is activated  */
-      public int m_iPitLimiter;
+      public int PitLimiter;
 
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szEngineMapping;
+      public string EngineMapping;
     }
 
     [StructLayoutAttribute(LayoutKind.Sequential)]
     public struct SPluginsBikeLap_t
     {
       /*  milliseconds  */
-      public int m_iLapTime;
+      public int LapTime;
 
       /*  1 = best lap  */
-      public int m_iBest;
+      public int Best;
 
-      public int m_iLapNum;
+      public int LapNum;
     }
 
     [StructLayoutAttribute(LayoutKind.Sequential)]
     public struct SPluginsBikeSplit_t
     {
-      public int m_iSplit;
+      public int Split;
 
       /*  milliseconds  */
-      public int m_iSplitTime;
+      public int SplitTime;
 
       /*  milliseconds.  Difference with best lap  */
-      public int m_iBestDiff;
+      public int BestDiff;
     }
 
     [StructLayoutAttribute(LayoutKind.Sequential)]
@@ -186,46 +186,46 @@ namespace EllieSpeed.Interfaces
     {
       /* 0,0 -> top left. 1,1 -> bottom right. counter-clockwise */
       [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 8, ArraySubType = UnmanagedType.R4)]
-      public float[] m_aafPos;
+      public float[] Pos;
 
-      /* 1 based index in SpriteName buffer. 0 = fill with m_ulColor */
-      public int m_iSprite;
+      /* 1 based index in SpriteName buffer. 0 = fill with Color */
+      public int Sprite;
 
       /* ABGR */
-      public uint m_ulColor;
+      public uint Color;
     }
 
     [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct SPluginString_t
     {
       [MarshalAsAttribute(UnmanagedType.ByValTStr, SizeConst = 100)]
-      public string m_szString;
+      public string String;
 
       /* 0,0 -> top left. 1,1 -> bottom right */
       [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 2, ArraySubType = UnmanagedType.R4)]
-      public float[] m_afPos;
+      public float[] Pos;
 
       /* 1 based index in FontName buffer */
-      public int m_iFont;
+      public int Font;
 
-      public float m_fSize;
+      public float Size;
 
       /* 0 = left; 1 = center; 2 = right */
-      public int m_iJustify;
+      public int Justify;
 
       /* ABGR */
-      public uint m_ulColor;
+      public uint Color;
     }
 
     [StructLayoutAttribute(LayoutKind.Sequential)]
     public struct SPluginsTrackSegment_t
     {
-      public int m_iType;
-      public float m_fLength;
-      public float m_fRadius;
-      public float m_fAngle;
+      public int Type;
+      public float Length;
+      public float Radius;
+      public float Angle;
       [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 2, ArraySubType = UnmanagedType.R4)]
-      public float[] m_afStart;
+      public float[] fStart;
     }
   }
 }
