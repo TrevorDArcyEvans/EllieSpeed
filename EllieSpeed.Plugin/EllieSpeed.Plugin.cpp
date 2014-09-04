f554bec7d264ad1dbebeb4d295991821eb5278b5
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-09-04 12:21:15 +0100

    - converted to extern C as compiled as C++ - removed logging

--- EllieSpeed.Plugin/EllieSpeed.Plugin.cpp
+++ EllieSpeed.Plugin/EllieSpeed.Plugin.cpp
@@ -10,32 +10,24 @@ If compiled as C++, extern "C" must be added to declaration of functions to expo
 X+ is right, Y+ is top and Z+ is forward.
 */
 
-__declspec(dllexport) char *GetModID()
+extern "C" char *GetModID()
 {
   return "gpbikes";
 }
 
-__declspec(dllexport) int GetModDataVersion()
+extern "C" int GetModDataVersion()
 {
   return 2;
 }
 
-__declspec(dllexport) int GetInterfaceVersion()
+extern "C" int GetInterfaceVersion()
 {
   return 8;
 }
 
-FILE *g_hTestLog;
-
 /* called when software is started */
-__declspec(dllexport) int Startup(char *_szSavePath)
+extern "C" int Startup(char *_szSavePath)
 {
-  g_hTestLog = fopen("gpbikes_log.txt","wt");
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Startup\n");
-  }
-
   /*
     return value is requested rate
     0 = 100hz; 1 = 50hz; 2 = 20hz; 3 = 10hz; -1 = disable
@@ -44,195 +36,59 @@ __declspec(dllexport) int Startup(char *_szSavePath)
 }
 
 /* called when software is closed */
-__declspec(dllexport) void Shutdown()
+extern "C" void Shutdown()
 {
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Shutdown\n");
-    fclose(g_hTestLog);
-    g_hTestLog = NULL;
-  }
 }
 
 /* called when event is initialized */
-__declspec(dllexport) void EventInit(void *_pData, int _iDataSize)
+extern "C" void EventInit(void *_pData, int _iDataSize)
 {
-  SPluginsBikeEvent_t *psEventData;
-
-  psEventData = (SPluginsBikeEvent_t*)_pData;
-
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Event init\n");
-    fprintf(g_hTestLog,"Bike %s\n",psEventData->m_szBikeName);
-    if (psEventData->m_fLimiter > 0)
-    {
-      fprintf(g_hTestLog,"Limiter: %d\n",(int)psEventData->m_fLimiter);
-    }
-    fprintf(g_hTestLog,"Max RPM: %d\n",(int)psEventData->m_fMaxRPM);
-    fprintf(g_hTestLog,"Category: %s\n",psEventData->m_szCategory);
-    fprintf(g_hTestLog,"Track: %s\n",psEventData->m_szTrackName);
-    fprintf(g_hTestLog,"Track Length: %d\n",(int)psEventData->m_fTrackLength);
-  }
+  SPluginsBikeEvent_t *psEventData = (SPluginsBikeEvent_t*)_pData;
 }
 
 /* called when bike goes to track */
-__declspec(dllexport) void RunInit(void *_pData, int _iDataSize)
+extern "C" void RunInit(void *_pData, int _iDataSize)
 {
-  SPluginsBikeSession_t *psSessionData;
-
-  psSessionData = (SPluginsBikeSession_t*)_pData;
-
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Run init\n");
-
-    switch (psSessionData->m_iSession)
-    {
-    case 0:
-      fprintf(g_hTestLog,"Testing\n");
-      break;
-    case 1:
-      fprintf(g_hTestLog,"Practice\n");
-      break;
-    case 2:
-      fprintf(g_hTestLog,"Qualify\n");
-      break;
-    case 3:
-      fprintf(g_hTestLog,"Warmup\n");
-      break;
-    case 4:
-      fprintf(g_hTestLog,"Race\n");
-      break;
-    }
-
-    switch (psSessionData->m_iConditions)
-    {
-    case 0:
-      fprintf(g_hTestLog,"Sunny\n");
-      break;
-    case 1:
-      fprintf(g_hTestLog,"Cloudy\n");
-      break;
-    case 2:
-      fprintf(g_hTestLog,"Rainy\n");
-      break;
-    }
-
-    fprintf(g_hTestLog,"Air temp: %f; Track temp: %f\n",psSessionData->m_fAirTemperature,psSessionData->m_fTrackTemperature);
-  }
+  SPluginsBikeSession_t *psSessionData = (SPluginsBikeSession_t*)_pData;
 }
 
 /* called when bike leaves the track */
-__declspec(dllexport) void RunDeinit()
+extern "C" void RunDeinit()
 {
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Run deinit\n");
-  }
 }
 
 /* called when simulation is started / resumed */
-__declspec(dllexport) void RunStart()
+extern "C" void RunStart()
 {
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Run start\n");
-  }
 }
 
 /* called when simulation is paused */
-__declspec(dllexport) void RunStop()
+extern "C" void RunStop()
 {
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Run stop\n");
-  }
 }
 
 /* called when a new lap is recorded. This function is optional */
-__declspec(dllexport) void RunLap(void *_pData, int _iDataSize)
+extern "C" void RunLap(void *_pData, int _iDataSize)
 {
-  SPluginsBikeLap_t *psLapData;
-
-  psLapData = (SPluginsBikeLap_t*)_pData;
-
-  if (g_hTestLog)
-  {
-    int iMinutes;
-    int iSeconds;
-    int iMilliseconds;
-
-    iMinutes = psLapData->m_iLapTime / 60000;
-    iSeconds = (psLapData->m_iLapTime / 1000) % 60;
-    iMilliseconds = psLapData->m_iLapTime % 1000;
-    if (iMinutes)
-    {
-      fprintf(g_hTestLog,"New lap: %d'%02d.%03d\n",iMinutes,iSeconds,iMilliseconds);
-    }
-    else
-    {
-      fprintf(g_hTestLog,"New lap: %d.%03d\n",iSeconds,iMilliseconds);
-    }
-  }
+  SPluginsBikeLap_t *psLapData = (SPluginsBikeLap_t*)_pData;
 }
 
 /* called when a split is crossed. This function is optional */
-__declspec(dllexport) void RunSplit(void *_pData, int _iDataSize)
+extern "C" void RunSplit(void *_pData, int _iDataSize)
 {
-  SPluginsBikeSplit_t *psSplitData;
-
-  psSplitData = (SPluginsBikeSplit_t*)_pData;
-
-  if (g_hTestLog)
-  {
-    int iMinutes;
-    int iSeconds;
-    int iMilliseconds;
-
-    iMinutes = psSplitData->m_iSplitTime / 60000;
-    iSeconds = (psSplitData->m_iSplitTime / 1000) % 60;
-    iMilliseconds = psSplitData->m_iSplitTime % 1000;
-    if (iMinutes)
-    {
-      fprintf(g_hTestLog,"Split %d: %d'%02d.%03d\n",psSplitData->m_iSplit + 1,iMinutes,iSeconds,iMilliseconds);
-    }
-    else
-    {
-      fprintf(g_hTestLog,"Split %d: %d.%03d\n",psSplitData->m_iSplit + 1,iSeconds,iMilliseconds);
-    }
-  }
+  SPluginsBikeSplit_t *psSplitData = (SPluginsBikeSplit_t*)_pData;
 }
 
 /* _fTime is the ontrack time, in seconds. _fPos is the position on centerline, from 0 to 1 */
-__declspec(dllexport) void RunTelemetry(void *_pData, int _iDataSize, float _fTime, float _fPos)
+extern "C" void RunTelemetry(void *_pData, int _iDataSize, float _fTime, float _fPos)
 {
-  SPluginsBikeData_t *psBikeData;
-
-  psBikeData = (SPluginsBikeData_t*)_pData;
-
-  if (g_hTestLog)
-  {
-    fprintf(g_hTestLog,"Position: %f%%\n",_fPos * 100);
-
-    fprintf(g_hTestLog,"RPM: %d\n",(int)psBikeData->m_fRPM);
-    fprintf(g_hTestLog,"Gear: %d\n",psBikeData->m_iGear);
-
-    fprintf(g_hTestLog,"Throttle: %d%%\n",(int)(psBikeData->m_fThrottle * 100));
-    fprintf(g_hTestLog,"Front brake: %d%%\n",(int)(psBikeData->m_fFrontBrake * 100));
-    fprintf(g_hTestLog,"Rear brake: %d%%\n",(int)(psBikeData->m_fRearBrake * 100));
-    fprintf(g_hTestLog,"Clutch: %d%%\n",(int)(psBikeData->m_fClutch * 100));
-
-    fprintf(g_hTestLog,"Front susp: %f\n",psBikeData->m_afSuspNormLength[0]);
-    fprintf(g_hTestLog,"Rear susp: %f\n",psBikeData->m_afSuspNormLength[1]);
-    fprintf(g_hTestLog,"Yaw: %f Pitch: %f Roll: %f\n",psBikeData->m_fYaw,psBikeData->m_fPitch,psBikeData->m_fRoll);
-
-    fprintf(g_hTestLog,"Speed: %d km/h\n",(int)(psBikeData->m_fSpeedometer * 3.6));
-  }
+  SPluginsBikeData_t *psBikeData = (SPluginsBikeData_t*)_pData;
 }
 
+#if 0
+
 /* called when software is started. This function is optional */
-__declspec(dllexport) int DrawInit(int *_piNumSprites, char **_pszSpriteName, int *_piNumFonts, char **_pszFontName)
+extern "C" int DrawInit(int *_piNumSprites, char **_pszSpriteName, int *_piNumFonts, char **_pszFontName)
 {
   /*
     return 0 if pointers are set
@@ -241,12 +97,12 @@ __declspec(dllexport) int DrawInit(int *_piNumSprites, char **_pszSpriteName, in
 }
 
 /* This function is optional */
-__declspec(dllexport) void TrackCenterline(int _iNumSegments, SPluginsTrackSegment_t *_pasSegment, void *_pRaceData)
+extern "C" void TrackCenterline(int _iNumSegments, SPluginsTrackSegment_t *_pasSegment, void *_pRaceData)
 {
 }
 
 /* This function is optional */
-__declspec(dllexport) void Draw(int *_piNumQuads, void **_ppQuad, int *_piNumString, void **_ppString)
+extern "C" void Draw(int *_piNumQuads, void **_ppQuad, int *_piNumString, void **_ppString)
 {
   *_piNumQuads = 0;
   *_ppQuad = NULL;
@@ -254,4 +110,5 @@ __declspec(dllexport) void Draw(int *_piNumQuads, void **_ppQuad, int *_piNumStr
   *_ppString = NULL;
 }
 
+#endif
 
