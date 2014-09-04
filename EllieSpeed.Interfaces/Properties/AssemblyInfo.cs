40fae42ffe679b007769e21cb3676d94d52e6941
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-09-04 16:12:35 +0100

    - made assy COM visible

--- EllieSpeed.Interfaces/Properties/AssemblyInfo.cs
+++ EllieSpeed.Interfaces/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("734c6b9a-1953-4d63-8d5a-fcb6a0bfa7ab")]
