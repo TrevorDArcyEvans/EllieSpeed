1b8c9293a9e99e366cab3fcac56ca9db67ff0e68
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2014-09-10 18:17:29 +0100

    - initial version

---
+++ EllieSpeed.Utilities/Utils.cs
@@ -0,0 +1,23 @@
+﻿//
+//  Copyright (C) 2014 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+using System.IO;
+
+namespace EllieSpeed.Utilities
+{
+  public class Utils
+  {
+    public static void SafeDelete(string filePath)
+    {
+      if (File.Exists(filePath))
+      {
+        File.Delete(filePath);
+      }
+    }
+  }
+}
