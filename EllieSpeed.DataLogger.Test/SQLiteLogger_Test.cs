78d531364f3c5ea9366c7ef119a234d242f3008c
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-09-10 17:23:56 +0100

    - initial version

---
+++ EllieSpeed.DataLogger.Test/SQLiteLogger_Test.cs
@@ -0,0 +1,27 @@
+﻿//
+//  Copyright (C) 2014 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace EllieSpeed.DataLogger.Test
+{
+  [TestFixture]
+  public class SQLiteLogger_Test
+  {
+    [Test]
+    public void Constructor_Completes()
+    {
+      var retVal = new SQLiteLogger("aaa");
+    }
+
+  }
+}
