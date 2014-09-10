f1266d15e60ed41672ca4fbd9a25007f341fd339
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2014-09-10 22:34:39 +0100

    - dummy Receiver event handlers - dispose of Logger + Receiver

--- EllieSpeed.DataLogger/SQLiteLogger.cs
+++ EllieSpeed.DataLogger/SQLiteLogger.cs
@@ -12,12 +12,15 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using EllieSpeed.Broadcast;
+using EllieSpeed.Receive;
 
 namespace EllieSpeed.DataLogger
 {
-  public class SQLiteLogger
+  public class SQLiteLogger : IDisposable
   {
     private readonly DataLogger mLogger;
+    private readonly Receiver mReceiver = new Receiver(Broadcaster.BroadcastPort);
 
     public SQLiteLogger(string filePath)
     {
@@ -40,6 +43,83 @@ namespace EllieSpeed.DataLogger
                           "provider=System.Data.SQLite;provider connection string=\"data source={0}\";", filePath);
 
       mLogger = new DataLogger(connStr);
+
+      mReceiver.OnStartup += OnStartup;
+      mReceiver.OnShutdown += OnShutdown;
+      mReceiver.OnEventInit += OnEventInit;
+      mReceiver.OnRunInit += OnRunInit;
+      mReceiver.OnRunDeinit += OnRunDeinit;
+      mReceiver.OnRunStart += OnRunStart;
+      mReceiver.OnRunStop += OnRunStop;
+      mReceiver.OnRunLap += OnRunLap;
+      mReceiver.OnRunSplit += OnRunSplit;
+      mReceiver.OnRunTelemetry += OnRunTelemetry;
+      mReceiver.OnTrackCenterline += OnTrackCenterline;
     }
+
+    public void OnStartup(object sender, EventArgs e)
+    {
+      Console.WriteLine("OnStartup");
+    }
+
+    void OnShutdown(object sender, EventArgs e)
+    {
+      Console.WriteLine("OnShutdown");
+    }
+
+    void OnEventInit(object sender, DataEventArgs<GPBikes.SPluginsBikeEvent_t> e)
+    {
+      Console.WriteLine("OnEventInit");
+    }
+
+    void OnRunInit(object sender, DataEventArgs<GPBikes.SPluginsBikeSession_t> e)
+    {
+      Console.WriteLine("OnRunInit");
+    }
+
+    void OnRunDeinit(object sender, EventArgs e)
+    {
+      Console.WriteLine("OnRunDeinit");
+    }
+
+    void OnRunStart(object sender, EventArgs e)
+    {
+      Console.WriteLine("OnRunStart");
+    }
+
+    void OnRunStop(object sender, EventArgs e)
+    {
+      Console.WriteLine("OnRunStop");
+    }
+
+    void OnRunLap(object sender, DataEventArgs<GPBikes.SPluginsBikeLap_t> e)
+    {
+      Console.WriteLine("OnRunLap");
+    }
+
+    void OnRunSplit(object sender, DataEventArgs<GPBikes.SPluginsBikeSplit_t> e)
+    {
+      Console.WriteLine("OnRunSplit");
+    }
+
+    void OnRunTelemetry(object sender, DataEventArgs<GPBikes.SPluginsBikeData_t> e)
+    {
+      Console.WriteLine("OnRunTelemetry");
+    }
+
+    void OnTrackCenterline(object sender, DataEventArgs<GPBikes.SPluginsTrackSegment_t[]> e)
+    {
+      Console.WriteLine("OnTrackCenterline");
+    }
+
+    #region IDisposable Members
+
+    public void Dispose()
+    {
+      mLogger.Dispose();
+      mReceiver.Dispose();
+    }
+
+    #endregion
   }
 }
