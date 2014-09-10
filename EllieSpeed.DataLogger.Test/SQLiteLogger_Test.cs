b44511c9a5f3f777d09d532c253ba96d7c8e3800
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2014-09-10 22:35:32 +0100

    - tests for Receiver events

--- EllieSpeed.DataLogger.Test/SQLiteLogger_Test.cs
+++ EllieSpeed.DataLogger.Test/SQLiteLogger_Test.cs
@@ -12,6 +12,8 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
+using EllieSpeed.Broadcast;
 using EllieSpeed.Utilities;
 using NUnit.Framework;
 
@@ -20,23 +22,143 @@ namespace EllieSpeed.DataLogger.Test
   [TestFixture]
   public class SQLiteLogger_Test
   {
-    [Test]
-    public void Constructor_Completes()
+    private string mDataFilePath;
+    private Broadcaster mBroadcaster;
+
+    [SetUp]
+    public void Setup()
     {
       var assyPath = Assembly.GetExecutingAssembly().Location;
       var assyDir = Path.GetDirectoryName(assyPath);
-      var dataFilePath = Path.Combine(assyDir, "DelMe.sqlite3");
-      Utils.SafeDelete(dataFilePath);
+      Utils.SafeDelete(mDataFilePath);
 
-      try
+      mDataFilePath = Path.Combine(assyDir, "DelMe.sqlite3");
+      mBroadcaster = new Broadcaster();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      Utils.SafeDelete(mDataFilePath);
+      mBroadcaster.Dispose();
+    }
+
+    [Test]
+    public void Constructor_Completes()
+    {
+      using (var logger = new SQLiteLogger(mDataFilePath))
       {
-        var logger = new SQLiteLogger(dataFilePath);
-
         Assert.IsNotNull(logger);
       }
-      finally
+    }
+
+    [Test]
+    public void OnStartup()
+    {
+      using (new SQLiteLogger(mDataFilePath))
       {
-        Utils.SafeDelete(dataFilePath);
+        mBroadcaster.OnStartup();
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnShutdown()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnShutdown();
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnEventInit()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnEventInit(new GPBikes.SPluginsBikeEvent_t());
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnRunInit()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnRunInit(new GPBikes.SPluginsBikeSession_t());
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnRunDeinit()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnRunDeinit();
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnRunStart()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnRunStart();
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnRunStop()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnRunStop();
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnRunLap()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnRunLap(new GPBikes.SPluginsBikeLap_t());
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnRunSplit()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnRunSplit(new GPBikes.SPluginsBikeSplit_t());
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnRunTelemetry()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnRunTelemetry(new GPBikes.SPluginsBikeData_t());
+        Thread.Sleep(500);
+      }
+    }
+
+    [Test]
+    public void OnTrackCenterline()
+    {
+      using (new SQLiteLogger(mDataFilePath))
+      {
+        mBroadcaster.OnTrackCenterline(new GPBikes.SPluginsTrackSegment_t[2]);
+        Thread.Sleep(500);
       }
     }
   }
