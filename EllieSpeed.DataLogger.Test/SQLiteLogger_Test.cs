ddaf956354f4cbfb02891cc5777501323d27a97a
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-09-11 14:37:37 +0100

    - unit tests for adding data to db

--- EllieSpeed.DataLogger.Test/SQLiteLogger_Test.cs
+++ EllieSpeed.DataLogger.Test/SQLiteLogger_Test.cs
@@ -14,6 +14,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using EllieSpeed.Broadcast;
+using EllieSpeed.Test.Utilties;
 using EllieSpeed.Utilities;
 using Moq;
 using NUnit.Framework;
@@ -25,6 +26,7 @@ namespace EllieSpeed.DataLogger.Test
   {
     private string mDataFilePath;
     private Broadcaster mBroadcaster;
+    private Mock<IBroadcaster> mMockBroadcaster = new Mock<IBroadcaster>();
 
     [SetUp]
     public void Setup()
@@ -34,6 +36,7 @@ namespace EllieSpeed.DataLogger.Test
       Utils.SafeDelete(mDataFilePath);
 
       mDataFilePath = Path.Combine(assyDir, "DelMe.sqlite3");
+      Utils.SafeDelete(mDataFilePath);
       mBroadcaster = new Broadcaster();
     }
 
@@ -56,113 +59,143 @@ namespace EllieSpeed.DataLogger.Test
     [Test]
     public void OnStartup()
     {
-      var mockBroadcaster = new Mock<IBroadcaster>();
-      using (new SQLiteLogger(mDataFilePath, mockBroadcaster.Object))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
         mBroadcaster.OnStartup();
-        Thread.Sleep(500);
+        Thread.Sleep(100);
 
-        mockBroadcaster.Verify(m => m.OnStartup(), Times.Once());
+        mMockBroadcaster.Verify(m => m.OnStartup(), Times.Once());
       }
     }
 
     [Test]
     public void OnShutdown()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
         mBroadcaster.OnShutdown();
-        Thread.Sleep(500);
+        Thread.Sleep(100);
+
+        mMockBroadcaster.Verify(m => m.OnShutdown(), Times.Once());
       }
     }
 
     [Test]
     public void OnEventInit()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      var data = TestUtils.CreateBikeEvent();
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
-        mBroadcaster.OnEventInit(new GPBikes.SPluginsBikeEvent_t());
-        Thread.Sleep(500);
+        mBroadcaster.OnEventInit(data);
+        Thread.Sleep(5000);
       }
+
+      mMockBroadcaster.Verify(m => m.OnEventInit(It.IsAny<GPBikes.SPluginsBikeEvent_t>()), Times.Once());
     }
 
     [Test]
     public void OnRunInit()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
-        mBroadcaster.OnRunInit(new GPBikes.SPluginsBikeSession_t());
-        Thread.Sleep(500);
+        var data = TestUtils.CreateBikeSession();
+        mBroadcaster.OnRunInit(data);
+        Thread.Sleep(5000);
+
+        mMockBroadcaster.Verify(m => m.OnRunInit(It.IsAny<GPBikes.SPluginsBikeSession_t>()), Times.Once());
       }
     }
 
     [Test]
     public void OnRunDeinit()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
         mBroadcaster.OnRunDeinit();
-        Thread.Sleep(500);
+        Thread.Sleep(100);
+
+        mMockBroadcaster.Verify(m => m.OnRunDeinit(), Times.Once());
       }
     }
 
     [Test]
     public void OnRunStart()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
         mBroadcaster.OnRunStart();
-        Thread.Sleep(500);
+        Thread.Sleep(100);
+
+        mMockBroadcaster.Verify(m => m.OnRunStart(), Times.Once());
       }
     }
 
     [Test]
     public void OnRunStop()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
         mBroadcaster.OnRunStop();
-        Thread.Sleep(500);
+        Thread.Sleep(100);
+
+        mMockBroadcaster.Verify(m => m.OnRunStop(), Times.Once());
       }
     }
 
     [Test]
     public void OnRunLap()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
-        mBroadcaster.OnRunLap(new GPBikes.SPluginsBikeLap_t());
-        Thread.Sleep(500);
+        var data = TestUtils.CreateBikeLap();
+        mBroadcaster.OnRunLap(data);
+        Thread.Sleep(5000);
+
+        mMockBroadcaster.Verify(m => m.OnRunLap(It.IsAny<GPBikes.SPluginsBikeLap_t>()), Times.Once());
       }
     }
 
     [Test]
     public void OnRunSplit()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
-        mBroadcaster.OnRunSplit(new GPBikes.SPluginsBikeSplit_t());
-        Thread.Sleep(500);
+        var data = TestUtils.CreateBikeSplit();
+        mBroadcaster.OnRunSplit(data);
+        Thread.Sleep(5000);
+
+        mMockBroadcaster.Verify(m => m.OnRunSplit(It.IsAny<GPBikes.SPluginsBikeSplit_t>()), Times.Once());
       }
     }
 
     [Test]
     public void OnRunTelemetry()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
-        mBroadcaster.OnRunTelemetry(new GPBikes.SPluginsBikeData_t());
+        var data = TestUtils.CreateBikeData();
+        mBroadcaster.OnRunTelemetry(data);
         Thread.Sleep(500);
+
+        mMockBroadcaster.Verify(m => m.OnRunTelemetry(It.IsAny<GPBikes.SPluginsBikeData_t>()), Times.Once());
       }
     }
 
     [Test]
     public void OnTrackCenterline()
     {
-      using (new SQLiteLogger(mDataFilePath))
+      using (new SQLiteLogger(mDataFilePath, mMockBroadcaster.Object))
       {
-        mBroadcaster.OnTrackCenterline(new GPBikes.SPluginsTrackSegment_t[2]);
+        var data = new[]
+                      {
+                        TestUtils.CreateTrackSegment(),
+                        TestUtils.CreateTrackSegment(),
+                        TestUtils.CreateTrackSegment()
+                      };
+        mBroadcaster.OnTrackCenterline(data);
         Thread.Sleep(500);
+
+        mMockBroadcaster.Verify(m => m.OnTrackCenterline(It.IsAny<GPBikes.SPluginsTrackSegment_t []>()), Times.Once());
       }
     }
   }
