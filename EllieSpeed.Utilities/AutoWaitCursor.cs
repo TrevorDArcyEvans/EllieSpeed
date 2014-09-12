b825a1375780626768a4134746025aea6a5767a2
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-09-12 14:44:22 +0100

    - added AutoCursor/AutoWaitCursor

---
+++ EllieSpeed.Utilities/AutoWaitCursor.cs
@@ -0,0 +1,20 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+using System.Windows.Forms;
+
+namespace EllieSpeed.Utilities
+{
+  public class AutoWaitCursor : AutoCursor
+  {
+    public AutoWaitCursor() :
+      base(Cursors.WaitCursor)
+    {
+    }
+  }
+}
