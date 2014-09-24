2d0535852635334edf917fefb27464797836e117
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2014-09-24 21:32:22 +0100

    - swallow exception which gets thrown when we pause the game

--- EllieSpeed.Receive/ReceiverBase.cs
+++ EllieSpeed.Receive/ReceiverBase.cs
@@ -10,6 +10,7 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace EllieSpeed.Receive
@@ -51,15 +52,26 @@ namespace EllieSpeed.Receive
 
     protected Object ByteArrayToObject(byte[] arrBytes)
     {
-      var memStream = new MemoryStream();
-      var bf = new BinaryFormatter();
+      try
+      {
+        using (var memStream = new MemoryStream())
+        {
+          var bf = new BinaryFormatter();
 
-      memStream.Write(arrBytes, 0, arrBytes.Length);
-      memStream.Seek(0, SeekOrigin.Begin);
+          memStream.Write(arrBytes, 0, arrBytes.Length);
+          memStream.Seek(0, SeekOrigin.Begin);
 
-      var obj = bf.Deserialize(memStream);
+          var obj = bf.Deserialize(memStream);
 
-      return obj;
+          return obj;
+        }
+      }
+      catch (SerializationException)
+      {
+        // getting exceptions when we pause game,
+        // so swallow these and return nothing
+        return null;
+      }
     }
 
     public void Dispose()
