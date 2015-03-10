88194def63f3bef5a8c24d9b6f05c72464231eb2
Trevor D'Arcy-Evans <trevor.darcy-evans@aveva.com>
2015-03-10 13:45:38 +0000

    -moved GPBikes structures into its own assembly

---
+++ EllieSpeed.GPBikes/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿//
+//  Copyright (C) 2015 EllieSpeed
+//
+//  All rights reserved
+//
+//  www.EllieSpeed.com
+//
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("EllieSpeed.GPBikes")]
+[assembly: AssemblyProduct("EllieSpeed.GPBikes")]
