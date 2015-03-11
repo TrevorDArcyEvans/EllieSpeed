c1118a02bc06b7df70dc84c1cadc23784b344cbf
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2015-03-11 22:12:09 +0000

    - more unit tests - changed import signatures to fix crash

--- EllieSpeed.Plugin.Input.Test/EllieSpeed.Plugin.Input.Test.cs
+++ EllieSpeed.Plugin.Input.Test/EllieSpeed.Plugin.Input.Test.cs
@@ -8,6 +8,7 @@
 
 using System;
 using System.Runtime.InteropServices;
+using EllieSpeed.GPBikes;
 using NUnit.Framework;
 
 namespace EllieSpeed.Plugin.Input.Test
@@ -48,12 +49,12 @@ namespace EllieSpeed.Plugin.Input.Test
 
     /// Return Type: int
     /* iIndex is the 0 based controller index. psInfo must be filled with controller info */
-    [DllImportAttribute("EllieSpeed.Plugin.Input.dli", EntryPoint = "GetControllerInfo")]
+    [DllImportAttribute("EllieSpeed.Plugin.Input.dli", EntryPoint = "GetControllerInfo", CallingConvention = CallingConvention.Cdecl)]
     public static extern int GetControllerInfo(int iIndex, IntPtr psInfo);
 
     /// Return Type: int
     /* iID is the unique controller ID. psData must be filled with controller data */
-    [DllImportAttribute("EllieSpeed.Plugin.Input.dli", EntryPoint = "GetControllerData")]
+    [DllImportAttribute("EllieSpeed.Plugin.Input.dli", EntryPoint = "GetControllerData", CallingConvention = CallingConvention.Cdecl)]
     public static extern int GetControllerData(int iID, IntPtr psData);
 
     #endregion
@@ -94,6 +95,43 @@ namespace EllieSpeed.Plugin.Input.Test
     public void GetNumControllers_ReturnsExpected()
     {
       Assert.AreEqual(GetNumControllers(), 1);
+    }
+
+    [Test]
+    public void GetControllerInfo_ReturnsExpected()
+    {
+      var info = new SControllerInfo_t
+                      {
+                        AxisRange = new short[18],
+                        SliderRange = new short[6],
+                        DialRange = new byte[8]
+                      };
+      var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(info));
+      Marshal.StructureToPtr(info, ptr, true);
+
+      GetControllerInfo(0, ptr);
+
+      // TODO   verify
+    }
+
+    [Test]
+    public void GetControllerData_ReturnsExpected()
+    {
+      Startup();
+      var data = new SControllerData_t
+                      {
+                        Axis = new short[6],
+                        Slider = new short[6],
+                        Button = new byte[32],
+                        POV = new byte[2],
+                        Dial = new byte[8]
+                      };
+      var ptr = Marshal.AllocHGlobal(Marshal.SizeOf(data));
+      Marshal.StructureToPtr(data, ptr, true);
+
+      GetControllerData(DataReceiver.ControllerID, ptr);
+
+      // TODO   verify
     }
   }
 }
