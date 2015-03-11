ec70db5f23bc28ca01aa90b3711530e2a1a5cfe7
Trevor D'Arcy-Evans <trevor.darcy-evans@aveva.com>
2015-03-11 17:05:04 +0000

    - added input plugin unit tests

---
+++ EllieSpeed.Plugin.Input.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿//
+//  Copyright (C) 2015 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("EllieSpeed.Plugin.Input.Test")]
+[assembly: AssemblyProduct("EllieSpeed.Plugin.Input.Test")]
